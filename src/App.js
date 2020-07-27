24a5bb20d3971a770b22c4b1e543f637167c59da
Iskilu Rasheed Akanni <akannirasheed1996@gmail.com>
2020-07-27 21:22:46 +0100

    A remodified Todo Web App using React.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+
+
+class Form extends Component {
+    constructor () {
+       super()
+       //initialize state
+       this.state = {
+            todo:'',
+            time:'',
+            todoList:[]
+       }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const { todoList, todo, time } = this.state //destructuring
+		const todoObject = {todo, time,}
+		
+		todoList.push(todoObject)
+		
+		this.setState({ todoList, todo:'', time:'' })
+    }
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    render() {
+        return (
+            <div id="man"> 
+				<h1 id="hi">To Do List App</h1>
+                <form class="done" onSubmit={this.handleSubmit}>
+                    <input name="todo" onChange={this.handleChange} placeholder="To do" value={this.state.todo}/>
+                    <input name="time" onChange={this.handleChange} placeholder="Time" value={this.state.time}/>
+                    <button type="submit">Add</button>
+                </form>
+                <ul>
+                    {this.state.todoList.map((item, index) => 
+                    <li style={{backgroundColor: index%2===0 ? 'orange' : 'blue'}} key={index}> <p className="todoItem">{item.todo}</p> 
+					<p>
+					{item.time}
+					</p></li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default Form;
